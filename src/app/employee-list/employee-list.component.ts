65703f8d12345533a809865ad966dd86e2ce56d1
VTischer <viktor.t1@schule.bremen.de>
2023-12-14 11:14:52 +0100

    Buttons in Table integriert

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -13,10 +13,20 @@ export class EmployeeListComponent {
   searchText: string = ' ';
   displayedColumns: string[] = ['Id', 'Name', 'Street', 'Postcode', 'City', 'Phone', 'Actions'];
   dataSource = [
-    { Id: 1, Name: 'John', Street: '123 Main St', Postcode: '12345', City: 'Example City', Phone: '555-1234', Actions: 'Edit | Delete' },
-    { Id: 2, Name: 'Alice', Street: '456 Oak St', Postcode: '67890', City: 'Sample Town', Phone: '555-5678', Actions: 'Edit | Delete' },
+    { Id: 1, Name: 'John', Street: '123 Main St', Postcode: '12345', City: 'Example City', Phone: '555-1234' },
+    { Id: 2, Name: 'Alice', Street: '456 Oak St', Postcode: '67890', City: 'Sample Town', Phone: '555-5678' },
     // Add more data as needed
   ];
+
+  editItem(item: any) {
+    // Handle edit action, e.g., navigate to edit page
+    console.log('Edit item:', item);
+  }
+
+  deleteItem(item: any) {
+    // Handle delete action, e.g., show confirmation dialog
+    console.log('Delete item:', item);
+  }
   employees$: Observable<Employee[]>;
 
   constructor(private http: HttpClient) {
