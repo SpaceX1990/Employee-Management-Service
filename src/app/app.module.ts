65daf4d2db824c8f5c3d3da8f75855cb7deb9603
VTischer <viktor.t1@schule.bremen.de>
2023-12-14 11:05:05 +0100

    Button's und Table Prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatTableModule} from "@angular/material/table";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -16,7 +21,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Observable, of} from "rxjs";
 import {Employee} from "../Employee";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-employee-list',
@@ -9,7 +10,13 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent {
-
+  searchText: string = ' ';
+  displayedColumns: string[] = ['Id', 'Name', 'Street', 'Postcode', 'City', 'Phone', 'Actions'];
+  dataSource = [
+    { Id: 1, Name: 'John', Street: '123 Main St', Postcode: '12345', City: 'Example City', Phone: '555-1234', Actions: 'Edit | Delete' },
+    { Id: 2, Name: 'Alice', Street: '456 Oak St', Postcode: '67890', City: 'Sample Town', Phone: '555-5678', Actions: 'Edit | Delete' },
+    // Add more data as needed
+  ];
   employees$: Observable<Employee[]>;
 
   constructor(private http: HttpClient) {
