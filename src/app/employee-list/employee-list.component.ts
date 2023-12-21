9cd1ae646234cc829e79239ac757cbaebba68744
VTischer <viktor.t1@schule.bremen.de>
2023-12-21 10:51:03 +0100

    Anpassungen

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -10,28 +10,6 @@ import {MatButton} from "@angular/material/button";
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent {
-  searchText: string = ' ';
-  displayedColumns: string[] = ['Id', 'Name', 'Street', 'Postcode', 'City', 'Phone', 'Actions'];
-  dataSource = [
-    { Id: 1, Name: 'John', Street: '123 Main St', Postcode: '12345', City: 'Example City', Phone: '555-1234' },
-    { Id: 2, Name: 'Alice', Street: '456 Oak St', Postcode: '67890', City: 'Sample Town', Phone: '555-5678' },
-    // Add more data as needed
-  ];
-
-  editItem(item: any) {
-    // Handle edit action, e.g., navigate to edit page
-    console.log('Edit item:', item);
-  }
-
-  deleteItem(item: any) {
-    // Handle delete action, e.g., show confirmation dialog
-    console.log('Delete item:', item);
-  }
-  viewItem(employee: any) {
-    // Implementieren Sie hier die Logik, um die Sichtbarkeit des Elements zu steuern.
-    console.log('View Item:', employee);
-    // Fügen Sie hier den Code hinzu, um die Sichtbarkeit zu steuern, z. B. API-Aufrufe oder lokale Logik.
-  }
   employees$: Observable<Employee[]>;
 
   constructor(private http: HttpClient) {
@@ -45,5 +23,30 @@ export class EmployeeListComponent {
         .set('Content-Type', 'application/json')
     });
   }
+  searchText: string = ' ';
+  displayedColumns: string[] = ['Id', 'Name', 'Street', 'Postcode', 'City', 'Phone', 'Actions'];
+  dataSource = [
+
+  ];
+
+  // createItem() {
+  //   console.log('CREATE Button wurde angeklickt!');
+  //   // Hier können Sie Ihre Logik hinzufügen
+  // }
+  //
+  // editItem(item: any) {
+  //   // Handle edit action, e.g., navigate to edit page
+  //   console.log('Edit item:', item);
+  // }
+  //
+  // deleteItem(item: any) {
+  //   // Handle delete action, e.g., show confirmation dialog
+  //   console.log('Delete item:', item);
+  // }
+  // viewItem(employee: any) {
+  //   // Implementieren Sie hier die Logik, um die Sichtbarkeit des Elements zu steuern.
+  //   console.log('View Item:', employee);
+  //   // Fügen Sie hier den Code hinzu, um die Sichtbarkeit zu steuern, z. B. API-Aufrufe oder lokale Logik.
+  // }
 
 }
