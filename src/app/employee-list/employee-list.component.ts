1b21afa92f9f6ad3a72ec48b3fe2611c4313b83c
dua <dua@neusta.de>
2023-12-21 11:46:43 +0100

    add html employeelist

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,9 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable, of} from "rxjs";
 import {Employee} from "../Employee";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Router} from "@angular/router";
-import {MatButton} from "@angular/material/button";
 import {EmployeeService} from "../service/employee.service";
 import {MatTableDataSource} from "@angular/material/table";
 
@@ -15,8 +13,7 @@ import {MatTableDataSource} from "@angular/material/table";
 export class EmployeeListComponent implements OnInit{
 
     dataSource = new MatTableDataSource<Employee>();
-    displayedColumns: string[] = ['id', 'firstName', 'lastName', 'street', 'postcode', 'city', 'phone'];
-    searchText: string = ' ';
+    displayedColumns: string[] = ['id', 'lastName', 'firstName',  'street', 'postcode', 'city', 'phone'];
 
     constructor(private http: HttpClient, private router: Router, private employeeService: EmployeeService) {
 
@@ -33,25 +30,4 @@ export class EmployeeListComponent implements OnInit{
             }
         )
     }
-
-    createItem() {
-      console.log('CREATE Button wurde angeklickt!');
-      // Hier können Sie Ihre Logik hinzufügen
-    }
-
-    editItem(item: any) {
-      // Handle edit action, e.g., navigate to edit page
-      console.log('Edit item:', item);
-    }
-
-    deleteItem(item: any) {
-      // Handle delete action, e.g., show confirmation dialog
-      console.log('Delete item:', item);
-    }
-    viewItem(employee: any) {
-      // Implementieren Sie hier die Logik, um die Sichtbarkeit des Elements zu steuern.
-      console.log('View Item:', employee);
-      // Fügen Sie hier den Code hinzu, um die Sichtbarkeit zu steuern, z. B. API-Aufrufe oder lokale Logik.
-    }
-
 }
