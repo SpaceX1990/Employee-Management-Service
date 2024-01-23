90e7b3822b0c7099f7cb459aaab90817851284d5
dua <dua@neusta.de>
2024-01-23 22:38:18 +0100

    refactor edit and details component

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -1,10 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
-import {HttpClient} from "@angular/common/http";
 import {RouterService} from "../../services/router.service";
 import {EmployeeModel} from "../../model/EmployeeModel";
-import {FORM_MODE} from "../../model/FormMode";
 import {EmployeeService} from "../../services/employee.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {NotificationService} from "../../services/notification.service";
 
 
 @Component({
@@ -12,30 +12,67 @@ import {EmployeeService} from "../../services/employee.service";
   templateUrl: './employee-details.component.html',
   styleUrls: ['./employee-details.component.css']
 })
-export class EmployeeDetailsComponent {
+export class EmployeeDetailsComponent implements OnInit {
+  employeeForm: FormGroup;
+  private employeeId: number;
+  private employeeModel: EmployeeModel;
 
-  constructor(private route:ActivatedRoute, private http: HttpClient, private routerService:RouterService,
-              private employeeService: EmployeeService) {
+  constructor(private routerService: RouterService,
+              private employeeService: EmployeeService,
+              private notificationService: NotificationService,
+              private fb: FormBuilder,
+              private route: ActivatedRoute
+  ) {
+    this.employeeForm = this.fb.group({
+      id: [{value: '', disabled: true}],
+      lastName: ['', Validators.required],
+      firstName: ['', Validators.required],
+      street: ['', Validators.required],
+      postcode: ['', [Validators.required, Validators.pattern(/^\d{5}$/)]],
+      city: ['', Validators.required],
+      phone: ['', [Validators.pattern(/^(\+?\d+[\s\-]*)+$/)]],
+    });
   }
 
-  detailsFormMode: FORM_MODE = FORM_MODE.DETAIL;
-  employeeId: number | null = null;
-  employeeDetails: EmployeeModel | null = null;
-
-  ngOnInit(){
+  ngOnInit() {
     let employeeIdParam = this.route.snapshot.paramMap.get(':employeeId');
-    if(employeeIdParam != null) {
+
+    if (employeeIdParam != null) {
       this.employeeId = Number.parseInt(employeeIdParam);
     }
-    if (this.employeeId != null){
+
+    if (this.employeeId != null) {
       this.employeeService.getById(this.employeeId).subscribe(data => {
-        this.employeeDetails = data;
+        this.employeeModel = data;
+        this.fillFormWithValues();
       });
     }
   }
 
+  fillFormWithValues() {
+    this.employeeForm.patchValue({
+      id: this.employeeModel.id,
+      lastName: this.employeeModel.lastName,
+      firstName: this.employeeModel.firstName,
+      street: this.employeeModel.street,
+      postcode: this.employeeModel.postcode,
+      city: this.employeeModel.city,
+      phone: this.employeeModel.phone,
+    });
+  }
+
   navToMainMenu() {
     this.routerService.navToEmployeeList();
+  }
+
+  updateEmployee() {
+    if (this.employeeForm.valid) {
+      const newEmployeeModel: EmployeeModel = this.employeeForm.value;
+      this.employeeService.updateById(this.employeeId, newEmployeeModel).subscribe(savedEmployee => {
+        this.notificationService.showSavedNotification();
+        this.routerService.navToEmployeeDetails(savedEmployee.id);
+      });
+    }
   }
 }
 
--- src/app/employee-edit/employee-edit.component.ts
+++ src/app/employee-edit/employee-edit.component.ts
@@ -1,49 +1,73 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {RouterService} from "../../services/router.service";
 import {EmployeeModel} from "../../model/EmployeeModel";
-import {FORM_MODE} from "../../model/FormMode";
 import {EmployeeService} from "../../services/employee.service";
 import {NotificationService} from "../../services/notification.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-employee-edit',
   templateUrl: './employee-edit.component.html',
   styleUrls: ['./employee-edit.component.css']
 })
-export class EmployeeEditComponent {
-  editFormMode: FORM_MODE = FORM_MODE.EDIT;
-  employeeId: number | null = null;
-  employeeDetails: EmployeeModel | null = null;
-  changedEmployeeDetails: EmployeeModel | null = null;
+export class EmployeeEditComponent implements OnInit {
+  employeeForm: FormGroup;
+  private employeeId: number;
+  private employeeModel: EmployeeModel;
 
-  constructor(private route:ActivatedRoute, private routerService:RouterService, private employeeService: EmployeeService,
-              private notificationService: NotificationService) {
+  constructor(private routerService: RouterService,
+              private employeeService: EmployeeService,
+              private notificationService: NotificationService,
+              private fb: FormBuilder,
+              private route: ActivatedRoute
+  ) {
+    this.employeeForm = this.fb.group({
+      id: [{value: '', disabled: true}],
+      lastName: ['', Validators.required],
+      firstName: ['', Validators.required],
+      street: ['', Validators.required],
+      postcode: ['', [Validators.required, Validators.pattern(/^\d{5}$/)]],
+      city: ['', Validators.required],
+      phone: ['', [Validators.pattern(/^(\+?\d+[\s\-]*)+$/)]],
+    });
   }
+
   ngOnInit() {
     let employeeIdParam = this.route.snapshot.paramMap.get(':employeeId');
+
     if (employeeIdParam != null) {
       this.employeeId = Number.parseInt(employeeIdParam);
     }
 
-    if (this.employeeId != null){
+    if (this.employeeId != null) {
       this.employeeService.getById(this.employeeId).subscribe(data => {
-        this.employeeDetails = data;
+        this.employeeModel = data;
+        this.fillFormWithValues();
       });
     }
+  }
+
+  fillFormWithValues() {
+    this.employeeForm.patchValue({
+      id: this.employeeModel.id,
+      lastName: this.employeeModel.lastName,
+      firstName: this.employeeModel.firstName,
+      street: this.employeeModel.street,
+      postcode: this.employeeModel.postcode,
+      city: this.employeeModel.city,
+      phone: this.employeeModel.phone,
+    });
   }
 
   navToMainMenu() {
     this.routerService.navToEmployeeList();
   }
 
-  onEmployeeChange(employeeDetails: EmployeeModel){
-    this.changedEmployeeDetails = employeeDetails;
-  }
-
-  updateEmployee(){
-    if(this.changedEmployeeDetails != null && this.employeeId != null){
-      this.employeeService.updateById(this.employeeId , this.changedEmployeeDetails).subscribe(savedEmployee => {
+  updateEmployee() {
+    if (this.employeeForm.valid) {
+      const newEmployeeModel: EmployeeModel = this.employeeForm.value;
+      this.employeeService.updateById(this.employeeId, newEmployeeModel).subscribe(savedEmployee => {
         this.notificationService.showSavedNotification();
         this.routerService.navToEmployeeDetails(savedEmployee.id);
       });
